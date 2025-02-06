aa212a63787de25814d54aa6c86a031331d168d2
rasul <rasulakhmeddibirov@gmail.com>
2025-02-06 19:36:01 +0100

    Init project

---
+++ src/main/java/com/glamvamp/GmalvampApplication.java
@@ -0,0 +1,13 @@
+package com.glamvamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmalvampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmalvampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glamvamp/GmalvampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glamvamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmalvampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
